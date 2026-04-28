25c4c6b0ca2ac5b8fa07f9b7874dcfc45f1bd3a6
GoldenWinter <121524712+Robotli89@users.noreply.github.com>
2026-04-28 16:25:36 -0400

    more changes

--- BoggleAssignment/Main.java
+++ BoggleAssignment/Main.java
@@ -20,6 +20,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Choose how to play Boggle:");
         System.out.println("1. GUI version");
+        System.out.println("2. Text version");
         System.out.print("Enter 1 or 2: ");
 
         String choice = input.nextLine().trim();
